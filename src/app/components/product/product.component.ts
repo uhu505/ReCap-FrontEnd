dfa4d83331d23d7a858a14064101cde8e2cc074c
uhureis <68116848+uhureis@users.noreply.github.com>
2021-03-28 13:43:38 +0300

    new commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { CarDetail } from 'src/app/modules/interface/car-detail';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,11 +12,35 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
 
-  carDetail:CarDetail[] = [];
+  carDetail:CarDetail[];
   dataLoaded = false;
-  constructor(private productService:ProductService) { }
+  number:number = 18;
+  filterText:string = "";
+  filterColorText:string = "";
+  
+  constructor(
+    private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService
+    ) { }
+
   ngOnInit(): void {
-    this.getAllProduct();
+    this.checkActivatedRoute();
+  }
+
+  checkActivatedRoute(){
+    this.activatedRoute.params.subscribe((params) => {
+       if(params["brandId"]){
+         this.getProductsByCategory(params["brandId"]);
+       } 
+       else if(params["colorId"]){
+         this.getProductsByColorCategory(params["colorId"]);
+       }
+       else{
+         this.getAllProduct();
+       }
+    });
   }
 
   getAllProduct(){
@@ -22,4 +49,24 @@ export class ProductComponent implements OnInit {
       this.dataLoaded = true;
     });
   }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
+      this.carDetail = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsByColorCategory(categoryId:number){
+    this.productService.getProductsByColorCategory(categoryId).subscribe((response) => {
+      this.carDetail = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  addToCart(car:CarDetail){
+    this.toastrService.success("Sepete Eklendi", car.brandName);
+    this.cartService.addToCart(car);
+  }
+
 }
